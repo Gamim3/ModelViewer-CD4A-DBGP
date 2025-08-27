5063285c7d78f1955f4a07f8c105945fb31fe5df
snoepkanon <78019309+snoepkanon@users.noreply.github.com>
2025-08-27 14:06:34 +0200

    panning, rotating and reset functionality
    de camereController heeft nu de mogelijk heid voor panning en rotatie functionalieted ook is er een reset toe gevoegd om alles naar de start positie te zetten

--- ModelViewer/Assets/Scripts/Controllers/CameraController.cs
+++ ModelViewer/Assets/Scripts/Controllers/CameraController.cs
@@ -7,33 +7,39 @@ public class CameraController : MonoBehaviour
 
     [Header("Sensitivity and movespeeds")]
     [SerializeField] private float _mouseSensitivity;
+    [SerializeField] private float _panSensitivity;
     [SerializeField] private float _zoomSpeed;
     [SerializeField] private float _panSpeed;
 
     [Header("Limits")]
-    [SerializeField] private Vector2 _panningLimits;
+    [SerializeField] private Vector4 _panningLimits;
     [SerializeField] private Vector2 _zoomLimits;
 
     private Vector2 _mouseInput;
     private float _scrollInput;
     private bool _mmbInput;
 
-    private float _targetZoomPos;
+    private float _targetZoomPos = -5f;
+    private Vector2 _targetPanPos;
 
     private Vector3 _defaultCamPos;
-    private Quaternion _defaultCamRot;
+    private Quaternion _defaultRot;
+    private float _defaultZoomPos;
+
+    private float _xRotation;
+    private float _yRotation;
 
 
     void Start()
     {
         _defaultCamPos = transform.position;
-        _defaultCamRot = transform.rotation;
-
-        _targetZoomPos = transform.localPosition.z;
+        _defaultRot = transform.rotation;
+        _defaultZoomPos = transform.localPosition.z;
     }
 
     void Update()
     {
+        HandleInput();
         HandleZoom();
     }
 
@@ -46,21 +52,45 @@ public class CameraController : MonoBehaviour
     {
         GetComponent<PlayerInput>().actions.FindAction("Zoom").performed -= CalculateZoom;
     }
-    private void HandlePanning()
-    {
 
+    private void HandleInput()
+    {
+        _mouseInput = GetComponent<PlayerInput>().actions.FindAction("Rotate").ReadValue<Vector2>();
+        if (GetComponent<PlayerInput>().actions.FindAction("AllowRotate").IsPressed())
+        {
+            HandleRotation();
+        }
+
+        if (GetComponent<PlayerInput>().actions.FindAction("Panning").IsPressed())
+        {
+            CalculatePanning();
+        }
+    }
+
+    private void CalculatePanning()
+    {
+        _targetPanPos.x -= _mouseInput.x * _panSensitivity;
+        _targetPanPos.y -= _mouseInput.y * _panSensitivity;
+        _targetPanPos.x = Mathf.Clamp(_targetPanPos.x, _panningLimits.x, _panningLimits.y);
+        _targetPanPos.y = Mathf.Clamp(_targetPanPos.y, _panningLimits.z, _panningLimits.w);
+        transform.localPosition = new Vector3(_targetPanPos.x, _targetPanPos.y, transform.localPosition.z);
     }
 
     private void HandleRotation()
     {
+        float mouseX = _mouseInput.x * _mouseSensitivity;
+        float mouseY = _mouseInput.y * _mouseSensitivity;
 
+        _xRotation -= mouseY;
+        _yRotation += mouseX;
+
+        _pivot.rotation = Quaternion.Euler(_xRotation, _yRotation, 0);
     }
 
     private void CalculateZoom(InputAction.CallbackContext ctx)
     {
-        Vector3 newPos = new Vector3(0, 0, ctx.ReadValue<Vector2>().y + transform.position.z);
+        Vector3 newPos = new Vector3(0, 0, ctx.ReadValue<Vector2>().y + transform.localPosition.z);
         _targetZoomPos = Mathf.Clamp(newPos.z, _zoomLimits.x, _zoomLimits.y);
-
     }
 
     private void HandleZoom()
@@ -68,10 +98,12 @@ public class CameraController : MonoBehaviour
         transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(transform.localPosition.x, transform.localPosition.y, _targetZoomPos), _zoomSpeed * Time.deltaTime);
     }
 
-    private void ResetCamera()
+    public void ResetCamera()
     {
+        _pivot.rotation = _defaultRot;
         transform.position = _defaultCamPos;
-        transform.rotation = _defaultCamRot;
+        _targetPanPos = new Vector2(0, 0);
+        _targetZoomPos = _defaultZoomPos;
     }
 
 }
