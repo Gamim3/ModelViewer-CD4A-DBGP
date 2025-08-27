5fa0cd4816ec174a459fd773d1f90e5324d86aa4
snoepkanon <78019309+snoepkanon@users.noreply.github.com>
2025-08-27 10:12:52 +0200

    UIManager model button spawning + model info refresh
    de UIManager kan nu de Model buttons met goede info (preview img, naam) spawnen en de gerelateerde model info in een textvlak zetten

--- ModelViewer/Assets/Scripts/Managers/UIManager.cs
+++ ModelViewer/Assets/Scripts/Managers/UIManager.cs
@@ -5,11 +5,17 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    private List<Button> _spawnedModelButtons;
-    private Button _modelUIBtn;//De prefab voor de model buttons
-    private RectTransform _modelButtonHolder;
-    private RectTransform _modelSelectionPanel;
-    private Animator _modelSelectionPanelAnimator;
+    private List<Button> _spawnedModelButtons = new();
+    [Header("Spawnable button info")]
+    [SerializeField] private Button _modelUIBtn;
+    [SerializeField] private TMP_Text _modelInfoText;
+    [Header("Model button location info")]
+    [SerializeField] private RectTransform _modelButtonHolder;
+    [SerializeField] private RectTransform _modelSelectionPanel;
+
+    [SerializeField] private Animator _modelSelectionPanelAnimator;
+
+    [SerializeField] private GameObject _loadingScreen;
 
     private void Awake()
     {
@@ -18,23 +24,29 @@ public class UIManager : MonoBehaviour
 
     private void OnEnable()
     {
-        ModelManager.Instance.OnModelChanged += RefreshModelInfo;
-        ModelManager.Instance.OnModelLoaded += SpawnButtons;
+        //TEMP uitgezet wegens geen model manager in test scene
+        //ModelManager.Instance.OnModelChanged += RefreshModelInfo;
+        //ModelManager.Instance.OnModelLoaded += SpawnButton;
     }
 
     private void OnDisable()
     {
         ModelManager.Instance.OnModelChanged -= RefreshModelInfo;
-        ModelManager.Instance.OnModelLoaded -= SpawnButtons;
+        ModelManager.Instance.OnModelLoaded -= SpawnButton;
     }
 
-    private void SpawnButtons(Model model)
+    /// <summary>
+    /// Spawns a button for the latest model that was preloaded
+    /// </summary>
+    /// <param name="model"></param>
+
+    public void SpawnButton(Model model)
     {
-            Button newButton = (Instantiate<Button>(_modelUIBtn, _modelButtonHolder.transform.position, _modelButtonHolder.transform.rotation));
-            newButton.GetComponentInChildren<TMP_Text>().text = model.modelName;
-            newButton.GetComponent<Image>().sprite = model.previewImage;
-            _spawnedModelButtons.Add(newButton);
-        
+        Button newButton = Instantiate(_modelUIBtn, _modelButtonHolder.transform.position, _modelButtonHolder.transform.rotation, _modelButtonHolder);
+        newButton.GetComponentInChildren<TMP_Text>().text = model.modelName;
+        newButton.GetComponent<Image>().sprite = model.previewImage;
+        RefreshModelInfo(model);//TEMP
+        _spawnedModelButtons.Add(newButton);
     }
 
     private void OnModelButtonClick()
@@ -47,12 +59,27 @@ public class UIManager : MonoBehaviour
         //call naar enviromentcontroller om de enviroment te veranderen
     }
 
-    private void RefreshModelInfo(Model modelinfo)
+    private void LoadScreen()
     {
-
+        //kijken hoe veel dingen en geladen moeten worden als het niet 0 dingen zijn laadscherm aan als het wel 0 dingen zijn laadscherm uit
     }
 
-    private void QuitApplication()
+    /// <summary>
+    /// Sets all the inforation of a model in the info UI based on last clicked model.
+    /// </summary>
+    /// <param name="modelinfo"></param>
+
+    private void RefreshModelInfo(Model modelinfo)
+    {
+        _modelInfoText.text = ("Naam model: " + modelinfo.modelName + "\n"
+            + "Omschrijving: " + modelinfo.description + "\n"
+            + "Naam artist: " + modelinfo.creatorName + "\n"
+            + "PollyCount = " + modelinfo.polyCount + "\n"
+            + "TriCount = " + modelinfo.triCount + "\n"
+            + "TextureCount = " + modelinfo.textureCount);
+    }
+
+    public void QuitApplication()
     {
         Application.Quit();
     }
